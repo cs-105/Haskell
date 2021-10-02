9f212c3631dc5cc417dc7c6c552923adede23a7a
Westmonty <lumontgomery@westmont.edu>
2021-10-02 15:33:31 -0700

    add beginnings of cabal hackage

---
+++ sweeper/CHANGELOG.md
@@ -0,0 +1,5 @@
+# Revision history for sweeper
+
+## 0.1.0.0 -- 2021-10-02
+
+* First version created. This is a great work in progress!
---
+++ sweeper/src/MyLib.hs
@@ -0,0 +1,10 @@
+module MyLib (someFunc) where
+
+import Cows (randomCow)
+import Data.Random.RVar (runRVar)
+import Data.Random.Source.DevRandom (DevRandom(DevURandom))
+import qualified Data.Text.IO as TIO
+
+
+someFunc :: IO ()
+someFunc = TIO.putStrLn =<< runRVar randomCow DevURandom
