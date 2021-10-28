efa1f70ce6b16a4f8ecba4a397620e7882f025c0
Luke Montgomery <lumontgomery@westmont.edu>
2021-10-27 17:11:10 -0700

    edit Main.hs in tests to include bulk testing

--- sweeper/test/Main.hs
+++ sweeper/test/Main.hs
@@ -2,5 +2,10 @@ import Test.HUnit
 
 test1 = TestCase( assertEqual "Testing tests by running them a lot: " "pass" "pass" )
 
-main = runTestTT test1
+allTests :: Test
+allTests = TestList [TestLabel "test1" test1]
+
+main :: IO Counts
+main = do _ <- runTestTT allTests
+          runTestTT allTests'
 
