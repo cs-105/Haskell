d3d5fe006af9219b4a3cf32ff57284f65108650e
Westmonty <lumontgomery@westmont.edu>
2021-11-08 19:20:26 -0800

    added comments for readability

--- sweeper/app/Main.hs
+++ sweeper/app/Main.hs
@@ -7,4 +7,5 @@ import System.Random
 
 main :: IO ()
 main = do
-  putStr . show =<< randomRIO (0, 100 :: Int)
+  putStr . show =<< randomRIO (1, 6 :: Int)
+  -- the above line simulated a die roll, generating an Int between 1 and 6 inclusive
