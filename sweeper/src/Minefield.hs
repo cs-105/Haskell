2b96341ebac57d96884bbf705a0743b22f2380e3
Westmonty <lumontgomery@westmont.edu>
2021-11-23 01:00:26 -0800

    restructure generate minefield to handle overarching IO

--- sweeper/src/Minefield.hs
+++ sweeper/src/Minefield.hs
@@ -2,10 +2,19 @@ module Minefield (generateMinefield) where
 import GhcPrelude (Enum)
 import Data.List (permutations)
 
-import System.Random
+import System.Random ()
 
-generateMinefield :: IO ()
-generateMinefield = putStrLn "dog"
+
+
+generateMinefield :: (Num a, Enum a) => a -> a -> Int -> Int -> IO [[a]]
+generateMinefield xSize ySize bombs seed = do
+    putStrLn "Please input a seed (positive integer):  "
+    y <- getLine
+    let intY = read y
+    safeGenerateMinefield xSize ySize bombs 5
+
+safeGenerateMinefield :: (Num a, Enum a) => a -> a -> Int -> Int -> [[a]]
+safeGenerateMinefield xSize ySize bombs seed = nBombsFromPool (shuffleBombs (allLocations xSize ySize) seed) bombs
 
 allLocations :: (Num a, Enum a) => a -> a -> [[a]]
 allLocations xSize ySize = [[x,y] | x <- [0..xSize-1], y <- [0..ySize-1]]
@@ -15,11 +24,14 @@ nBombsFromPool potentialBombs n
     | n <= 1    = [head potentialBombs]
     | otherwise = [head potentialBombs] ++ (nBombsFromPool (tail potentialBombs) (n - 1))
 
-shuffleBombs :: (Num a, Enum a) => [[a]] -> [[a]]
-shuffleBombs bombPool = 
-    do
-        g <- newStdGen
-        permutations bombPool !! (randomR (0, length bombPool) g)
+shuffleBombs :: (Num a, Enum a) => [[a]] -> Int -> [[a]]
+shuffleBombs bombPool seed = permutations bombPool !! seed
+    -- do
+    --     putStrLn "Please input a seed (positive integer):  "
+    --     y <- getLine
+    --     let intY = read y
+    --     permutations bombPool !! 1
+
 
 -- when this is all done, generateMinefield will be as follows:
 -- generateMinefield xSize ySize bombs = nBombsFromPool (nBombsFromPool (allLocations xSize ySize) bombs)
