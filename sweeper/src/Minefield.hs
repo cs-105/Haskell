558143da4ba45e8109340e4b050e7139f1c62615
Westmonty <lumontgomery@westmont.edu>
2021-11-28 21:34:18 -0800

    remove unnecessary comments and fixed seed issue with shuffleBombs

--- sweeper/src/Minefield.hs
+++ sweeper/src/Minefield.hs
@@ -24,21 +24,10 @@ allLocations xSize ySize = [[x,y] | x <- [0..xSize-1], y <- [0..ySize-1]]
 nBombsFromPool :: [[Int]] -> Int -> [[Int]]
 nBombsFromPool potentialBombs n
     | n <= 1    = [head potentialBombs]
-    | otherwise = [head potentialBombs] ++ (nBombsFromPool (tail potentialBombs) (n - 1))
+    | otherwise = (head potentialBombs) : (nBombsFromPool (tail potentialBombs) (n - 1))
 
 shuffleBombs :: [[Int]] -> Int -> [[Int]]
-shuffleBombs bombPool seed = permutations bombPool !! seed
-    -- do
-    --     putStrLn "Please input a seed (positive integer):  "
-    --     y <- getLine
-    --     let intY = read y
-    --     permutations bombPool !! 1
-
+shuffleBombs bombPool seed = permutations bombPool !! (seed `mod` (length (permutations bombPool)))
 
 -- when this is all done, generateMinefield will be as follows:
 -- generateMinefield xSize ySize bombs = nBombsFromPool (nBombsFromPool (allLocations xSize ySize) bombs)
-
-
-    -- do
-    --     canvas <- [(i,j) | i <- [0,a], j <- [0,b]]
-    --     putStr . show =<< randomRIO (a, b :: Int)
